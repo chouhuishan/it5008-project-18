cbe5d6e8f94ffb567ce55845f8ad092265f701da
Chou Hui Shan <huishaan@live.com>
2025-09-01 14:35:41 +0800

    codes for submission

--- project_1/codes/order.sql
+++ project_1/codes/order.sql
@@ -1,15 +1,15 @@
 TRUNCATE TABLE restaurant.order_item;
 TRUNCATE TABLE restaurant.order;
-INSERT INTO restaurant.order
-VALUES (
-        '2024-03-01 12:19:23',
-        '20240301002',
-        'card',
-        '5108-7574-2920-6803',
-        'mastercard',
-        14,
-        '93627414'
-    );
+-- INSERT INTO restaurant.order
+-- VALUES (
+--         '2024-03-01 12:19:23',
+--         '20240301002',
+--         'card',
+--         '5108-7574-2920-6803',
+--         'mastercard',
+--         14,
+--         '93627414'
+--     );
 INSERT INTO restaurant.order_item
 VALUES (
         '20240301002',
@@ -22,3 +22,5 @@ VALUES (
         'Ayam Balado',
         'STAFF-04'
     );
+
+-- 
